310c48ad60bf89abf7d5521e6bc15201c56a6b57
Emil Stanimirov <145929264+EmilStanimirov90@users.noreply.github.com>
2023-11-20 01:27:51 +0200

    exercises

---
+++ 01_first_stpes_in_OOP/08_pokemon_battle_exercise/project/trainer.py
@@ -0,0 +1,44 @@
+from typing import List
+from project.pokemon import Pokemon
+
+
+class Trainer:
+    def __init__(self, name: str):
+        self.name = name
+        self.pokemons: List[Pokemon] = []
+
+    def add_pokemon(self, pokemon: Pokemon):
+        if pokemon.name in [p.name for p in self.pokemons]:
+            return "This pokemon is already caught"
+
+        self.pokemons.append(pokemon)
+        return f"Caught {pokemon.pokemon_details()}"
+
+    def release_pokemon(self, pokemon_name: str):
+        for p_obj in self.pokemons:
+            if p_obj.name == pokemon_name:
+                self.pokemons.remove(p_obj)
+                return f"You have released {pokemon_name}"
+        return f"Pokemon is not caught"
+
+    def trainer_data(self):
+        result = ''
+        result += f"Pokemon Trainer {self.name}\n"
+        result += f"Pokemon count {len(self.pokemons)}\n"
+        for poke in self.pokemons:
+            result += f"- {poke.pokemon_details()}"
+        return result
+
+# TEST CODE:
+
+#
+# pokemon = Pokemon("Pikachu", 90)
+# print(pokemon.pokemon_details())
+# trainer = Trainer("Ash")
+# print(trainer.add_pokemon(pokemon))
+# second_pokemon = Pokemon("Charizard", 110)
+# print(trainer.add_pokemon(second_pokemon))
+# print(trainer.add_pokemon(second_pokemon))
+# print(trainer.release_pokemon("Pikachu"))
+# print(trainer.release_pokemon("Pikachu"))
+# print(trainer.trainer_data())
