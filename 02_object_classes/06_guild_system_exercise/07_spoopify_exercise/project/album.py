4015a46e29fe617e6241044aa68ab43a01f0273f
Emil Stanimirov <145929264+EmilStanimirov90@users.noreply.github.com>
2023-11-22 00:09:14 +0200

    exercise

---
+++ 02_object_classes/06_guild_system_exercise/07_spoopify_exercise/project/album.py
@@ -0,0 +1,12 @@
+from song import Song
+class Album:
+    def __init__(self, name: str, *songs):
+        self.name = name
+        self.songs = songs
+        self.published = False
+        self.songs = []
+
+    def add_song(self, song: Song):
+        if song not in self.songs:
+            self.songs.append(song)
+        if
