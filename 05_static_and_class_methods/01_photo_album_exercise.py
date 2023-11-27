09aae11dbcefa347ebb18df36ee2a0ea0914754e
Emil Stanimirov <145929264+EmilStanimirov90@users.noreply.github.com>
2023-11-27 22:11:01 +0200

    exercise

---
+++ 05_static_and_class_methods/01_photo_album_exercise.py
@@ -0,0 +1,41 @@
+import math
+
+
+class PhotoAlbum:
+    def __init__(self, pages):
+        self.pages = pages
+        self.photos = [[] for _ in range(pages)]
+
+    @classmethod
+    def from_photos_count(cls, photos_count: int):
+        return cls(math.ceil(photos_count / 4))
+
+    def add_photo(self, label):
+        for index, page in enumerate(self.photos):
+            if len(page) < 4:
+                page.append(label)
+                return f"{label} photo added successfully on page {index + 1} slot {len(page)}"
+        return "No more free slots"
+
+    def display(self):
+        separator = "-" * 11 + "\n"
+        result = separator
+        for page in self.photos:
+            result += ' '.join(["[]" for el in page]) + "\n"
+            result += separator
+        return result.strip()
+
+
+# test code:
+
+album = PhotoAlbum(2)
+
+print(album.add_photo("baby"))
+print(album.add_photo("first grade"))
+print(album.add_photo("eight grade"))
+print(album.add_photo("party with friends"))
+print(album.photos)
+print(album.add_photo("prom"))
+print(album.add_photo("wedding"))
+
+print(album.display())
